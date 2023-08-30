79d36ebb758e544997a32c5f0144e91df81c4760
Surachotkit <surachot.kit@gmail.com>
2023-08-30 14:12:02 +0700

    save toggle

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,7 @@ import {
   FaInbox,
   FaCalendar,
   FaCalendarAlt,
-  FaChevronDown,
+  FaChevronDown
 } from "react-icons/fa";
 import Header from "../components/Header";
 import ListItem from "../components/ListItem";
@@ -18,29 +18,31 @@ function App() {
         <aside className="sidebar">
           <section className="sidebar__cartegory">
             <ul className="list">
-              <ListItem text="Inbox" icon={<FaInbox className="list__item__icon"/>} />
-              <ListItem text="Today" icon={<FaCalendar className="list__item__icon"/>} />
-              <ListItem text="Next 7 Days" icon={<FaCalendarAlt className="list__item__icon"/>} />
-              {/* <li className="list__item">
-                    <FaInbox className="list__item__icon"/>
-              
-                <p className="list__item__text">Inbox</p>
-              </li>
-              <li className="list__item">
-            
-                    <FaCalendar className="list__item__icon"/>
-              
-                <p className="list__item__text">Today</p>
-              </li>
-              <li className="list__item">
-            
-                    <FaCalendarAlt className="list__item__icon"/>
-              
-                <p className="list__item__text">Next 7 Days</p>
-              </li> */}
+              <ListItem text="Inbox" icon={<FaInbox />} active={true} />
+              <ListItem text="Today" icon={<FaCalendar />} active={false} />
+              <ListItem
+                text="Next 7 Days"
+                icon={<FaCalendarAlt />}
+                active={false}
+              />
             </ul>
           </section>
-          <section className="sidebar__cartegory">2</section>
+          <section className="sidebar__cartegory">
+            <div className="accordion">
+              {/* Toggle */}
+              <div className='accordion__toggle'>
+                <li className='accordion__item'>
+                  <FaChevronDown className='accordion__item__icon accordion__item__active' />
+                  <p className='accordion__item__text'>Projects</p>
+                </li>
+              </div>
+              {/* List */}
+              <ul className="lists">
+                <ListItem text="Project-A" icon={<FaInbox />} active={true} />
+                <ListItem text="Project-B" icon={<FaInbox />} active={false} />
+              </ul>
+            </div>
+          </section>
         </aside>
       </div>
       <div className="todo__content">TodoContent</div>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -5,16 +5,28 @@ import {
   FaCalendarAlt,
   FaChevronDown,
 } from "react-icons/fa";
-
+/**
+ props = {
+    text : string
+    icon : <Component/>
+    active: boolean
+ }
+ */
 function ListItem(props) {
   console.log(props);
+  const listClassName = `list__item ${props.active ? "active" : ""}`;
+  // active = fasle -> textClassName = "list__item"
+  // active = true ->textClassName = "list__item active"
   return (
-    <li className="list__item">
+    <li className={listClassName}>
       {props.icon}
 
-      <p className="list__item__text">{props.text}</p>
+      <p className='list__item__text'>{props.text}</p>
     </li>
   );
 }
 
+// CSS + JS Value == Dynamics ClassName
+// not-active : className="list__item"
+// active : className="list__item active"
 export default ListItem;
