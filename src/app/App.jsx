d546f896ca129f3c282890852df0730b3067cbed
Surachotkit <surachot.kit@gmail.com>
2023-08-30 14:49:58 +0700

    refactor

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,12 +3,26 @@ import {
   FaInbox,
   FaCalendar,
   FaCalendarAlt,
-  FaChevronDown
+  FaChevronDown,
 } from "react-icons/fa";
 import Header from "../components/Header";
 import ListItem from "../components/ListItem";
 
 function App() {
+  //<ListItem text="Inbox" icon={<FaInbox />} active={true} />
+
+  const generalLists = [
+    { id: 1, text: "inbox",icon: <FaInbox />, active: true },
+    { id: 2, text: "Today",icon: <FaCalendar />, active: false },
+    { id: 3, text: "Next 7 Days",icon: <FaCalendarAlt />, active: false },
+  ];
+
+  //<ListItem text="Project-A" icon={<FaInbox />} active={true} />
+  //<ListItem text="Project-B" icon={<FaInbox />} active={false} />
+  const projectLists =[
+    { id:4, text: "Project-A", icon:<FaInbox/>, active:true},
+    { id:5, text: "Project-B", icon:<FaInbox/>, active:false},
+  ]
   return (
     <div className="todo">
       <div className="todo__header">
@@ -18,28 +32,24 @@ function App() {
         <aside className="sidebar">
           <section className="sidebar__cartegory">
             <ul className="list">
-              <ListItem text="Inbox" icon={<FaInbox />} active={true} />
-              <ListItem text="Today" icon={<FaCalendar />} active={false} />
-              <ListItem
-                text="Next 7 Days"
-                icon={<FaCalendarAlt />}
-                active={false}
-              />
+              {generalLists.map((obj) => <ListItem key={obj.id} text={obj.text} icon={obj.icon} active={obj.active}/> )}
+
+
             </ul>
           </section>
           <section className="sidebar__cartegory">
             <div className="accordion">
               {/* Toggle */}
-              <div className='accordion__toggle'>
-                <li className='accordion__item'>
-                  <FaChevronDown className='accordion__item__icon accordion__item__active' />
-                  <p className='accordion__item__text'>Projects</p>
+              <div className="accordion__toggle">
+                <li className="accordion__item">
+                  <FaChevronDown className="accordion__item__icon accordion__item__active" />
+                  <p className="accordion__item__text">Projects</p>
                 </li>
               </div>
               {/* List */}
               <ul className="lists">
-                <ListItem text="Project-A" icon={<FaInbox />} active={true} />
-                <ListItem text="Project-B" icon={<FaInbox />} active={false} />
+                {projectLists.map((obj) => <ListItem key={obj.id} text={obj.text} icon={obj.icon} active={obj.active} />)}
+
               </ul>
             </div>
           </section>
