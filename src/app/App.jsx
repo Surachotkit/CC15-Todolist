be9a6d624789bab3fabe391af7045fc85f1fc4cf
Surachotkit <surachot.kit@gmail.com>
2023-08-30 11:54:58 +0700

    finish: sidebar ui

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import "../app/App.scss";
-import { FaHome } from 'react-icons/fa';
+import { FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
 import Header from "../components/Header";
 
 function App() {
@@ -8,7 +8,33 @@ function App() {
       <div className="todo__header">
         <Header/>
       </div>
-      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__sidebar">
+        <aside className="sidebar">
+          <section className="sidebar__cartegory">
+            <ul className="list">
+              <li className="list__item">
+            
+                    <FaInbox className="list__item__icon"/>
+              
+                <p className="list__item__text">Inbox</p>
+              </li>
+              <li className="list__item">
+            
+                    <FaCalendar className="list__item__icon"/>
+              
+                <p className="list__item__text">Today</p>
+              </li>
+              <li className="list__item">
+            
+                    <FaCalendarAlt className="list__item__icon"/>
+              
+                <p className="list__item__text">Next 7 Days</p>
+              </li>
+            </ul>
+          </section>
+          <section className="sidebar__cartegory">2</section>
+        </aside>
+      </div>
       <div className="todo__content">TodoContent</div>
     </div>
   );
