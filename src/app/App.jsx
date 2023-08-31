38edd733cdb9aaa04d80ccf1909a612728bcc1a4
Surachotkit <surachot.kit@gmail.com>
2023-08-31 09:21:04 +0700

    UI Complete

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,83 +1,26 @@
-import "../app/App.scss";
-import {
-  FaInbox,
-  FaCalendar,
-  FaCalendarAlt,
-  FaChevronDown,
-} from "react-icons/fa";
-import Header from "../components/Header";
-import ListItem from "../components/ListItem";
-import Lists from "../components/Lists";
-import TodoHeader from "../components/Todo/TodoHeader";
-import TodoCreate from "../components/Todo/TodoCreate";
-import TodoLists from "../components/Todo/TodoLists";
+// Dependencies
+import './App.scss';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
+import TodoHeader from '../components/Todo/TodoHeader';
+import TodoCreate from '../components/Todo/TodoCreate';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
 
 function App() {
-  //<ListItem text="Inbox" icon={<FaInbox />} active={true} />
-
-  const generalLists = [
-    { id: 1, text: "inbox",icon: <FaInbox />, active: true },
-    { id: 2, text: "Today",icon: <FaCalendar />, active: false },
-    { id: 3, text: "Next 7 Days",icon: <FaCalendarAlt />, active: false },
-  ];
-
-  //<ListItem text="Project-A" icon={<FaInbox />} active={true} />
-  //<ListItem text="Project-B" icon={<FaInbox />} active={false} />
-  const projectLists =[
-    { id:4, text: "Project-A", icon:<FaInbox/>, active:true},
-    { id:5, text: "Project-B", icon:<FaInbox/>, active:false},
-  ]
   return (
-    <div className="todo">
-      <div className="todo__header">
-        <Header />
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
       </div>
-      <div className="todo__sidebar">
-        <aside className="sidebar">
-          <section className="sidebar__cartegory">
-
-            <Lists data={generalLists} />
-
-            {/* <ul className="list">
-              {generalLists.map((obj) => <ListItem key={obj.id} text={obj.text} icon={obj.icon} active={obj.active}/> )}
-            </ul> */}
-          </section>
-          <section className="sidebar__cartegory">
-            <div className="accordion">
-              {/* Toggle */}
-              <div className="accordion__toggle">
-                <li className="accordion__item">
-                  <FaChevronDown className="accordion__item__icon accordion__item__active" />
-                  <p className="accordion__item__text">Projects</p>
-                </li>
-              </div>
-              {/* List */}
-
-              <Lists data={projectLists} />
-
-              {/* <ul className="lists">
-                {projectLists.map((obj) => <ListItem key={obj.id} text={obj.text} icon={obj.icon} active={obj.active} />)}
-              </ul> */}
-            </div>
-          </section>
-        </aside>
+      <div className='todo__sidebar'>
+        <SideBar />
       </div>
-      <div className="todo__content">
-        <main className="todo__container">
-              {/* Header */}
-              <TodoHeader/>
-              {/* <div className="header">
-                <h1 className="header__text">Inbox</h1>
-                <span className="header__date"></span>
-              </div> */}
-
-
-              {/* CreateTodo */}
-              <TodoCreate/>
-              
-              {/* ToodoList */}
-              <TodoLists/>
-
+      <div className='todo__content'>
+        <main className='todo__container'>
+          <TodoHeader />
+          <TodoCreate />
+          <TodoLists />
         </main>
       </div>
     </div>
@@ -85,4 +28,3 @@ function App() {
 }
 
 export default App;
-
